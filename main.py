a42aefe651f2934bb9d35652e920effbeec1846c
TRudenko22 <timothy.rudenkom@gmail.com>
2022-01-16 15:23:45 -0500

    added ascii to spice up the look

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
-import time
 from rich import print as rprint
+from ascii import * 
+from os import system
+import time
 import simpleaudio as sa
 
-you_suffer = sa.WaveObject.from_wave_file("Napalm Death - You Suffer.wav")
+you_suffer = sa.WaveObject.from_wave_file('Napalm Death - You Suffer.wav')
 
-minutes = int(input("\nEnter Working Minutes: "))*60
-rminutes = int(input("Enter Resting Minutes: "))*60
+system('clear')
+minutes = int(input('\nEnter Working Minutes: '))
+rminutes = int(input('Enter Resting Minutes: '))
+rounds = int(input('Enter Rounds: '))
 
-print("\n")
-while minutes:
-    mins, secs = divmod(minutes, 60)
-    timer = f"[cyan]Working: {'{:02d}:{:02d}'.format(mins, secs)}[/cyan]"
-    rprint(timer, end="\r")
-    time.sleep(1)
-    minutes -= 1
+system('clear')
+for loop in range(rounds):
+    
+    lMinutes1 = minutes * 60
+    lMinutes2 = rminutes * 60
 
+    rprint(f'\n{working}\n')
+    while lMinutes1: 
+        mins, secs = divmod(lMinutes1, 60)
+        timer = f"\t\t[cyan][b]     Working: {'{:02d}:{:02d}'.format(mins, secs)}[/b][/cyan]"
+        rprint(timer, end='\r')
+        time.sleep(1)
+        lMinutes1 -= 1
 
-print("\n")    
-you_suffer.play()
-
-
-while rminutes:
-    mins, secs = divmod(rminutes, 60)
-    timer = f"[cyan]Resting: {'{:02d}:{:02d}'.format(mins, secs)}[/cyan]"
-    rprint(timer, end="\r")
-    time.sleep(1)
-    rminutes -= 1
- 
-
+    you_suffer.play()
+    system('clear')
+    
+    rprint(f'\n{resting}\n')
+    while lMinutes2:
+        mins, secs = divmod(lMinutes2, 60)
+        timer = f"\t\t[cyan][b]     Resting: {'{:02d}:{:02d}'.format(mins, secs)}[/b][/cyan]"
+        rprint(timer, end='\r')
+        time.sleep(1)
+        lMinutes2 -= 1
+    
+    print('\n')    
+    you_suffer.play()
+    system('clear')
